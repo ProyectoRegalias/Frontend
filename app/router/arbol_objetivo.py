381bcab903319073fe3bed5e7b7b3aae57dc5a24
james rojas <jamesfr8764@hotmail.com>
2024-11-24 14:20:30 -0500

    ajustando tablas objetivos y causas, navegacion entre ellas y funcionalidades en botones de las tablas

--- app/router/arbol_objetivo.py
+++ app/router/arbol_objetivo.py
@@ -7,14 +7,27 @@ from app.utils.utils import chat_
 arbol_objetivo = Blueprint('arbol_objetivo', __name__)
 
 
-@arbol_objetivo.route('/arbolobjetivo', methods=['POST'])
-def arbol_objective():
-    json = request.get_json()
-    fines_directos = (json.get("fines_directas")).split(',')
-    fines_indirectos = (json.get("fines_indirectas")).split(',')
-    objetivos_especificos = (json.get("objetivos_especificos")).split(',')
-    medios = (json.get("medios")).split(',')
-    print("HOLA")
-    # Renderiza la página del árbol de problemas con los datos
+@arbol_objetivo.route('/arbolobjetivos')
+def arbolobjetivos():
+    return render_template('arbol_objetivos.html')
+
+
+@arbol_objetivo.route('/arbolobjetivo', methods=['POST', 'GET'])
+def arbolo_bjetivo():
+    if request.method == 'POST':
+        json = request.get_json()
+        session['fines_directos'] = (json.get("fines_directas")).split(',')
+        session['fines_indirectos'] = (json.get("fines_indirectas")).split(',')
+        session['objetivos_especificos'] = (json.get("objetivos_especificos")).split(',')
+        session['medios'] = (json.get("medios")).split(',')
+        return 'ENTRO', 204  # Respuesta sin contenido
+
+    if request.method == 'GET':
+        fines_directos = session.get('fines_directos', [])
+        fines_indirectos = session.get('fines_indirectos', [])
+        objetivos_especificos = session.get('objetivos_especificos', [])
+        medios = session.get('medios', [])
+
     return render_template('arbol_objetivos.html', problema="problema", fines_directos=fines_directos,
-                           fines_indirectos=fines_indirectos, objetivos_especificos=objetivos_especificos, medios=medios)
+                           fines_indirectos=fines_indirectos, objetivos_especificos=objetivos_especificos,
+                           medios=medios)
